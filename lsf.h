e822e09077f5f325f83c9284c151bf75b95767bf
Caleb Donovick <cdonovi1@binghamton.edu>
2015-05-05 19:24:27 -0400

    create file

---
+++ lsf.h
@@ -0,0 +1,20 @@
+#ifndef LSF_H
+#define LSF_H
+
+class LogFileSystem {
+    public:
+        /* 
+         * capacity     - the capacity in bytes of the file system
+         * segment_size - the size in bytes of a segment
+         * block_size   - the size in bytes of a block
+         *
+         * clean        - ratio
+         */
+        LogFileSystem(unsigned fs_capacity, 
+                unsigned segment_size, 
+                unsigned block_size,
+
+
+};
+#endif
+
