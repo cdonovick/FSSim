31f4ada355ba4e2760eecc3a838240db9c354dbc
Caleb Donovick <cdonovi1@binghamton.edu>
2015-05-06 00:19:37 -0400

    Do work

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "standard.h"
 #include "logfilesystem.h"
+#include "segment.h"
 
 int main(int argc, char **argv) {
 
--- segment.h
+++ segment.h
@@ -12,16 +12,16 @@ class Segment {
         
         bool isDirty(void) const noexcept;
         double getLiveliness(void) const noexcept;
-
+        SegmentNumber getSegmentNumber() const noexcept;
 
         void setBlock(BlockNumber n, Block b);
         Block getBlock(BlockNumber n) const;
-        SegmentNumber getSegmentNumber;
     private:
+        const SegmentNumber m_idx;
+        bool m_is_dirty;
+
         std::vector<Block> m_blocks;
         Segment *m_next;
-        bool m_is_dirty;
-        SegmentNumber m_idx;
 };
 #endif
 
